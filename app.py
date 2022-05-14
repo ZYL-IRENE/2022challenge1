6e5a31bbd166dc5e49141304efdc463caf872f5d
pikachu~ <71221586+AaronZhao99@users.noreply.github.com>
2022-05-14 23:38:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,21 +11,25 @@ def hello_world():
 @app.route('/data-info3-view', methods = ['GET', 'POST'])
 def get_info3_view_data():
     data_node, data_link = utils.get_data_info3()
-    idx = "子图1-1"
-    if request.method == 'POST':
-        idx = request.post.get("graphTypes")
     nodes_all = ['Domain', 'IP', 'Cert', 'Whois_Name', 'Whois_Phone', 'Whois_Email', 'IP_C', 'ASN']
     links_all = ['VeryStrong', 'Strong', 'Ordinary', 'Weak']
-    node_list = []
-    link_list = []
-    for value,name in  zip(data_node[int(idx[-1])-1],nodes_all):
-        temp_dict = {'value': value, 'name':name}
-        node_list.append(temp_dict)
-    for value,name in  zip(data_link[int(idx[-1])-1],links_all):
-        temp_dict = {'value': value, 'name':name}
-        link_list.append(temp_dict)
-    return jsonify({"node":node_list, "link":link_list})
 
+
+    res_node = []
+    res_link = []
+    for i in range(len(data_node)):
+        node_list = []
+        link_list = []
+        for value, name in zip(data_node[i], nodes_all):
+            temp_dict = {'value': value, 'name': name}
+            node_list.append(temp_dict)
+        for value, name in zip(data_link[i], links_all):
+            temp_dict = {'value': value, 'name': name}
+            link_list.append(temp_dict)
+        res_node.append(node_list)
+        res_link.append(link_list)
+    print(res_node)
+    return jsonify({"node": res_node, "link": res_link})
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='127.0.0.1', port=7000)
