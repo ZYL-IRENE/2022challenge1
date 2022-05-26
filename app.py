a8312c9933700d08c7c57da7a401f4ae36a18235
pikachu~ <71221586+AaronZhao99@users.noreply.github.com>
2022-05-26 10:01:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,27 +8,33 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
-@app.route('/data-info3-view', methods = ['GET', 'POST'])
-def get_info3_view_data():
-    data_node, data_link = utils.get_data_info3()
-    nodes_all = ['Domain', 'IP', 'Cert', 'Whois_Name', 'Whois_Phone', 'Whois_Email', 'IP_C', 'ASN']
-    links_all = ['VeryStrong', 'Strong', 'Ordinary', 'Weak']
+@app.route('/subgraph-info1-view', methods = ['GET', 'POST'])
+def get_info6_view_data():
+    graph6_data_node, graph6_data_link = utils.get_data_info6()
+    graph6_nodes_all = ['Domain', 'IP', 'Cert', 'Whois_Name', 'Whois_Phone', 'Whois_Email', 'IP_C', 'ASN']
+    graph6_links_all = ['VeryStrong', 'Strong', 'Ordinary', 'Weak']
 
-
-    res_node = []
-    res_link = []
-    for i in range(len(data_node)):
+    graph6_res_node = []
+    graph6_res_link = []
+    for i in range(len(graph6_data_node)):
         node_list = []
         link_list = []
-        for value, name in zip(data_node[i], nodes_all):
+        for value, name in zip(graph6_data_node[i], graph6_nodes_all):
             temp_dict = {'value': value, 'name': name}
             node_list.append(temp_dict)
-        for value, name in zip(data_link[i], links_all):
+        for value, name in zip(graph6_data_link[i], graph6_links_all):
             temp_dict = {'value': value, 'name': name}
             link_list.append(temp_dict)
-        res_node.append(node_list)
-        res_link.append(link_list)
-    return jsonify({"node": res_node, "link": res_link})
+        graph6_res_node.append(node_list)
+        graph6_res_link.append(link_list)
+    print(graph6_res_node)
+    return jsonify({"node": graph6_res_node, "link": graph6_res_link})
 
+@app.route('/subgraph-info2-view', methods = ['GET', 'POST'])
+def get_info7_view_data():
+    graph7_data_node, graph7_data_link, graph7_data_category = utils.get_data_info7()
+    print(graph7_data_node)
+    return jsonify({"node": graph7_data_node, "link": graph7_data_link, "category": graph7_data_category})
+# get_info7_view_data()
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=7000)
