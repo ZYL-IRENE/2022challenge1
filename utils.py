f737f928b597e18db7c49e00d3d0dc082f6d7083
pikachu~ <71221586+AaronZhao99@users.noreply.github.com>
2022-05-26 10:01:51 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,8 +2,8 @@ import json
 import os
 import pandas as pd
 
-def get_data_info3():
-    path = './static/data/data_info3'
+def get_data_info6():
+    path = './static/data/data_info6'
     count = 0
     for file in os.listdir(path):  # file 表示的是文件名
         count = count + 1
@@ -17,4 +17,15 @@ def get_data_info3():
         pd_link_types = pd.read_csv(file_path2)
         res_link[i] = pd_link_types['num'].values.tolist()
     return res_node,res_link
-get_data_info3()
+get_data_info6()
+
+def get_data_info7():
+    path = './static/data/data_info7/1-temp.json'
+    with open(path,'r') as f:
+        info7_data = json.load(f)
+    res_node = info7_data['nodes']
+    res_link = info7_data['links']
+    res_category = info7_data['category']
+    return res_node,res_link,res_category
+get_data_info7()
+# print(get_data_info7())
