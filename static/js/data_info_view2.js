6211099501ccc72581903d26fc64c416dc065ea2
pikachu~ <71221586+AaronZhao99@users.noreply.github.com>
2022-06-02 21:57:56 +0800

    Update data_info_view2.js

--- static/js/data_info_view2.js
+++ static/js/data_info_view2.js
@@ -5,9 +5,9 @@ var info2_colors = ['#749F83', '#65757D'];
 var info2_option = {
     color: info2_colors,
     tooltip: {
-        trigger: 'axis', 
-        axisPointer: { 
-            type: 'shadow' 
+        trigger: 'axis',
+        axisPointer: {
+            type: 'shadow'
         }
     },
     legend: {
@@ -15,11 +15,11 @@ var info2_option = {
         top:'0%'
     },
     grid: {
-        left: '3%', 
-        right: '4%', 
+        left: '3%',
+        right: '4%',
         bottom: '15%',
         top:'15%',
-        containLabel: true 
+        containLabel: true
     },
     textStyle: '13',
 
@@ -28,38 +28,38 @@ var info2_option = {
         bottom: '5%',
         height: '20px',
         start:0,
-        end:60 
+        end:60
     },
     xAxis: [{
         type: 'category',
-        data: ['子图1-1', '子图1-2', '子图1-3', '子图1-4', '子图1-5', '子图2-1', '子图2-2', '子图2-3', '子图2-4', '子图2-5'],
+        data: ['子图1', '子图2', '子图3', '子图4', '子图5', '子图6', '子图7', '子图8', '子图9', '子图10','子图11'],
         axisTick: {
             alignWithLabel: true
         },
         axisLabel: {
-            rotate: 45 
+            rotate: 45
         },
     }],
     yAxis: [{
         type: 'value'
     }],
-    series: [{ 
-            name: 'node', 
+    series: [{
+            name: 'node',
             type: 'bar',
             itemStyle: {
-                barBorderRadius: [10, 10, 0, 0] 
+                barBorderRadius: [10, 10, 0, 0]
             },
-            barWidth: '30%', 
-            data: [110, 152, 200, 334, 390, 530, 1220, 1434, 1367, 1200]
+            barWidth: '30%',
+            data: [286, 311, 934, 1518, 732, 432, 265, 202, 592, 1553, 938]
         },
-        { 
+        {
             name: 'link',
             type: 'bar',
             itemStyle: {
-                barBorderRadius: [10, 10, 0, 0] 
+                barBorderRadius: [10, 10, 0, 0]
             },
             barWidth: '30%',
-            data: [310, 466, 598, 1289, 1370, 2310, 2200, 2134, 3467, 4132, 3123, 4240]
+            data: [649, 1459, 1099, 3576, 1512, 881, 415, 268, 881, 2651, 1670]
         },
     ]
 };
